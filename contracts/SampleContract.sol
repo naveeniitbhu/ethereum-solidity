82ba20b21b10eb1af5ac9d5b34c897a47f2df902
naveen securra <naveen@securrahealth.com>
2025-12-04 14:47:10 +0530

    Update: Fallback modified

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.14;
+pragma solidity 0.8.15;
 
 contract SampleContract {
     string public myString = "Hello World";
+
+    constructor() payable {
+
+    }
 
     function updateString(string memory _newString) public payable {
         if(msg.value == 1 ether) {
--- contracts/SampleFallback.sol
+++ contracts/SampleFallback.sol
@@ -5,6 +5,18 @@ pragma solidity 0.8.15;
 contract SampleFallback {
     uint public lastValueSent;
     string public lastFunctionCalled;
+
+    uint public myUint;
+
+    function setMyUint(uint _myNewUint) public {
+        myUint = _myNewUint;
+    }
+
+    // 0xe492fd840000000000000000000000000000000000000000000000000000000000000001
+
+    // funciton signature "setMyUint(uint256)"
+
+    // 0xe492fd842fb25dc4b3c624c80776108b452a545c682a78e4252b5560c6537b79
  
     receive() external payable {
         lastValueSent = msg.value;
