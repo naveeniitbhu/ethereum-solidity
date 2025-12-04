df87c554e04f9b137ee157dd773785e620148796
naveen securra <naveen@securrahealth.com>
2025-12-04 23:43:41 +0530

    Update: Error handling

---
+++ contracts/ExampleTryCatch.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract WillThrow {
+    error NotAllowedError(string);
+    function aFunction() public pure {
+        // require(false, "Error msg");
+        // assert(false);
+        revert NotAllowedError("You are not allowed");
+    }
+}
+
+contract ErrorHandling {
+    event ErrorLogging(string reason);
+    event ErrorLogCode(uint code);
+    event ErrorLogBytes(bytes lowLevelData);
+
+    function catchTheError() public {
+        WillThrow will = new WillThrow();
+        try will.aFunction() {
+            // code
+        } catch Error(string memory reason) {
+            emit ErrorLogging(reason);
+        } catch Panic(uint errorCode) {
+            emit ErrorLogCode(errorCode);
+        } catch (bytes memory lowLevelData) {
+            emit ErrorLogBytes(lowLevelData);
+        }
+    }
+}
+
+// lowLeveldata - has signature and data - web3.utils.toAscii("...."
